757f014d4c171a0365296af5be66c371aa4047a8
choi yejun <yejun4911@naver.com>
2021-09-13 21:09:16 +0900

    backend

---
+++ backend/src/test/java/com/ca1endar/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ca1endar.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
